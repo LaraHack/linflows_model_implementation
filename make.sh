b1f3848b0bb2e1c652c28e6a6096fd028d526bc6
Tobias Kuhn <kuhntobias@gmail.com>
2019-11-29 12:40:48 +0100

    Update script

--- make.sh
+++ make.sh
@@ -5,5 +5,6 @@ mkdir -p virtuoso/data/virtuoso/dumps
 rm virtuoso/data/virtuoso/dumps/*.nq
 TIMESTAMP=$(date '+%Y%m%d%H%M%S')
 cat article_*/nanopubs_*.trig > temp.trig
-./np mktrusty -r -o virtuoso/data/virtuoso/dumps/trusty.nanopubs.$TIMESTAMP.nq temp.trig
+./np mktrusty -r -o trusty.nanopubs.trig temp.trig
+np op filter -o virtuoso/data/virtuoso/dumps/trusty.nanopubs.$TIMESTAMP.nq trusty.nanopubs.trig
 rm temp.trig
