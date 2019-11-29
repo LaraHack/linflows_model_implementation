937463f15fa2e951c3445ee5d266dca672f5759a
Tobias Kuhn <kuhntobias@gmail.com>
2019-11-29 12:19:03 +0100

    Improve scripts

--- make.sh
+++ make.sh
@@ -2,8 +2,8 @@
 
 sudo chmod 777 virtuoso/data/virtuoso/dumps
 mkdir -p virtuoso/data/virtuoso/dumps
-rm virtuoso/data/virtuoso/dumps/*.trig
+rm virtuoso/data/virtuoso/dumps/*.nq
 TIMESTAMP=$(date '+%Y%m%d%H%M%S')
 cat article_*/nanopubs_*.trig > temp.trig
-./np mktrusty -r -o virtuoso/data/virtuoso/dumps/trusty.nanopubs.$TIMESTAMP.trig temp.trig
+./np mktrusty -r -o virtuoso/data/virtuoso/dumps/trusty.nanopubs.$TIMESTAMP.nq temp.trig
 rm temp.trig
