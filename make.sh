48e91673882853c823acf4582ce7b6d4d14a460b
Tobias Kuhn <kuhntobias@gmail.com>
2019-11-29 12:03:33 +0100

    Fix reloading

--- make.sh
+++ make.sh
@@ -2,6 +2,8 @@
 
 sudo chmod 777 virtuoso/data/virtuoso/dumps
 mkdir -p virtuoso/data/virtuoso/dumps
+rm virtuoso/data/virtuoso/dumps/*.trig
+TIMESTAMP=$(date '+%Y%m%d%H%M%S')
 cat article_*/nanopubs_*.trig > temp.trig
-./np mktrusty -r -o virtuoso/data/virtuoso/dumps/trusty.nanopubs.trig temp.trig
+./np mktrusty -r -o virtuoso/data/virtuoso/dumps/trusty.nanopubs.$TIMESTAMP.trig temp.trig
 rm temp.trig
